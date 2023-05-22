ae7b7dc3cc33149188a5f44b49fa1ab2dffdba1c
Antonedei Wonimidei Otoro <antonedei.otoro@engineer.com>
2023-05-21 19:22:54 -0500

    created the contract model

--- backend/models/contractModel.js
+++ backend/models/contractModel.js
@@ -1,7 +1,21 @@
 const mongoose = require("mongoose");
 
-const ContractSchema = new mongoose.Schema({
-    
-}, { timestamps: true });
+const ContractSchema = new mongoose.Schema(
+  {
+    user_id: String,
+    project_id: String,
+    contract_type: String,
+    participants: [
+      {
+        participant_id: String,
+        skill: String,
+        percentage_offered: Number,
+      },
+    ],
+    contract_status: String,
+    contract_start_date: Date,
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("contract", ContractSchema);
